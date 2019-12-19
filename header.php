b087d02432b55b8af2438dda556e803ad5f42061
Sharan Chakradhar <40011522+SirSharanChakradhar@users.noreply.github.com>
2019-12-19 14:30:34 +1100

    new files
    These are updated new files, projects goes ahead from here.

---
+++ header.php
@@ -0,0 +1,121 @@
+<?php
+  // First we start a session which allow for us to store information as SESSION variables.
+  session_start();
+  // "require" creates an error message and stops the script. "include" creates an error and continues the script.
+  require "includes/dbh.inc.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="description" content="renting">
+    <meta name="author" content="guest Inventory">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>GI</title>
+
+
+    <link href="./assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link type="text/css" href="./assets/bds.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/pure.css">
+    <link rel="stylesheet" href="./assets/bds.css">
+    <link rel="stylesheet" href="./assets/docsify.css">
+    <style type="text/css">
+    .medium-zoom-overlay {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        opacity: 0;
+        transition: opacity .3s;
+        will-change: opacity
+    }
+
+    .medium-zoom--opened .medium-zoom-overlay {
+        cursor: pointer;
+        cursor: zoom-out;
+        opacity: 1
+    }
+
+    .medium-zoom-image {
+        cursor: pointer;
+        cursor: zoom-in;
+        transition: transform .3s cubic-bezier(.2, 0, .2, 1)
+    }
+
+    .medium-zoom-image--hidden {
+        visibility: hidden
+    }
+
+    .medium-zoom-image--opened {
+        position: relative;
+        cursor: pointer;
+        cursor: zoom-out;
+        will-change: transform
+    }
+    </style>
+</head>
+
+
+
+<body>
+
+    <!-- Here is the header where I decided to include the login form for this tutorial. -->
+
+    <aside class="sidebar">
+        <div class="sidebar-nav">
+            <ul>
+                <li>
+                    <p><a href="index.php" class="docsify-logo">
+                            <img src="./assets/svg/biings-ds-logo.svg" data-origin="./assets/svg/biings-ds-logo.svg"
+                                alt="" data-no-zoom="">
+                        </a></p>
+                </li>
+                <div class="menu is-small">
+                    <ul class="menu-list">
+                        <p></p>
+                        <li>
+                        <p><a href="signup.php">Signup</a></p>
+                        </li>
+                        <li>
+                        <p><a href="login.php">Login</a></p>
+                        </li>
+                                              
+                    </ul>
+                </div>
+                
+            </ul>
+        </div>
+    </aside>
+
+    <!-- Page content -->
+    <div class="main">
+
+
+
+        <!--
+    Here is the HTML login form.
+    Notice that the "method" is set to "post" because the data we send is sensitive data.
+    The "inputs" I decided to have in the form include username/e-mail and password. The user will be able to choose whether to login using e-mail or username.
+
+    Also notice that using PHP, we can choose whether or not to show the login/signup form, or to show the logout form, if we are logged in or not. We do this based on SESSION variables which I explain in more detail in the login.inc.php file!
+    -->
+        <?php
+    if (!isset($_SESSION['id'])) {
+      echo '<a href="login.php" class="btn btn-warning btn-icon ml-sm-3 d-none d-md-block">
+      <span class="btn-inner--icon">
+      <i class="fa fa-sign-in mr-3"></i>Login</a>';
+    }
+    else if (isset($_SESSION['id'])) {
+      echo '<a href="includes/logout.inc.php" class="btn btn-neutral btn-icon ml-sm-3 d-none d-md-block"><span class="btn-inner--icon"><i class="fas fa-hiking mr-3"></i>Logout</a>';
+    }
+    ?>
+
+        ...
+    </div>
+</body>
---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+  // To make sure we don't need to create the header section of the website on multiple pages, we instead create the header HTML markup in a separate file which we then attach to the top of every HTML page on our website. In this way if we need to make a small change to our header we just need to do it in one place. This is a VERY cool feature in PHP!
+  require "header.php";
+ 
+?>
+
+<section class="content is-narrow">
+
+    <div class="markdown-section">
+        <h1 class="title is-1 is-family-secondary">Login</h1>
+        <hr class="is-visible is-size-3">
+        <!--
+        Here is the HTML login form.
+        Notice that the "method" is set to "post" because the data we send is sensitive data.
+        The "inputs" I decided to have in the form include username/e-mail and password. The user will be able to choose whether to login using e-mail or username.
+
+        Also notice that using PHP, we can choose whether or not to show the login/signup form, or to show the logout form, if we are logged in or not. We do this based on SESSION variables which I explain in more detail in the login.inc.php file!
+        -->
+        <?php
+        if (!isset($_SESSION['id'])) 
+        {
+          echo '
+          <form action="includes/login.inc.php" method="post">
+          <div class="box is-well">
+            <input class="input" type="text" name="mailuid" placeholder="E-mail/Username">
+            <hr class="is-size-8">
+            <input class="input" type="password" name="pwd" placeholder="Password">
+            <hr class="is-size-8">
+            <button class="button is-dark" type="submit" name="login-submit">Login</button>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            
+            <button class="button is-light" href="signup.php" class="header-signup"><strong> No Account? &nbsp;💁‍♂️ Signup !! </strong></button>
+            </div>
+          </form>
+          ';
+        }
+        else if (isset($_SESSION['id']))
+         {
+          echo '<form action="includes/logout.inc.php" method="post">
+          <div class="box is-well">
+          <hr class="is-size-8">
+            <button class="button is-dark" type="submit" name="login-submit">Logout</button>
+            </div>
+          </form>';
+        }
+        ?>
+    </div>
+</section>
+
+<?php
+  // And just like we include the header from a separate file, we do the same with the footer.
+  require "footer.php";
+?>
